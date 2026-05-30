ce863c5143eb1810432099d68a3f7b6287d1a800
Kanak Sharma <kanaksharma0512.ks@gmail.com>
2026-05-30 14:13:07 +0530

    Added image upload API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"message": "Consumer Health AI Backend Running"}
+
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+
+    allowed = [".jpg", ".jpeg", ".png"]
+
+    ext = os.path.splitext(file.filename)[1].lower()
+
+    if ext not in allowed:
+        return {"error": "Only JPG and PNG images allowed"}
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "message": "Image uploaded successfully"
+    }
